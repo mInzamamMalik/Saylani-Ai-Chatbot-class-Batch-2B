5cb07bc22ffa69ba942e42db61ab8136de4614ed
M.Inzamam Malik <malikasinger1@gmail.com>
2022-10-23 22:33:43 +0500

    deploy on cloud

---
+++ 2. nodejs/3. deloyment on cloud/server.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/water', (req, res) => {
+    console.log(`${req.ip} is asking for water`)
+    res.send('Here is some water for you')
+})
+app.get('/food', (req, res) => {
+    console.log(`${req.ip} is asking for food`)
+    res.send('Here is some food for you')
+})
+//  172.16.19.78:3000/water
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
