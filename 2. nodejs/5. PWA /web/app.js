104b0cc6c17c8afc7ff8c9f1ea1b59210e3524ca
M.Inzamam Malik <malikasinger1@gmail.com>
2023-01-01 22:37:12 +0500

    pwa class code

---
+++ 2. nodejs/5. PWA /web/app.js
@@ -0,0 +1,110 @@
+
+const url = window.location.href;
+let baseUrl = "";
+
+if (url.split(":")[0] === 'https') {
+    baseUrl = '';
+} else {
+    baseUrl = 'http://localhost:5001';
+}
+
+
+
+
+
+
+let addProduct = () => {
+
+    let name = document.querySelector("#name").value
+    let price = document.querySelector("#price").value
+    let cat = document.querySelector("#cat").value
+    let desc = document.querySelector("#desc").value
+
+    axios.post(`${baseUrl}/product`, {
+        name: name,
+        price: price,
+        category: cat,
+        description: desc
+    })
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data);
+
+            document.querySelector("#result").innerHTML =
+                response.data.message
+
+            getAllProducts();
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            document.querySelector("#result").innerHTML =
+                error.data.message
+        })
+
+}
+let getAllProducts = () => {
+
+    console.log("get all product ======>");
+    axios.get(`${baseUrl}/products`, {
+        // query URL without using browser cache
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Expires': '0',
+        },
+    })
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data.data);
+            document.querySelector("#productList").innerHTML = ""
+
+            response?.data?.data.map((eachProduct, index) => {
+                document.querySelector("#productList").innerHTML +=
+                    `
+                    <div>
+                        <h1>${eachProduct.name} </h1>
+                        <p>${eachProduct.price} </p>
+                        <p>${eachProduct.category} </p>
+                        <p>${eachProduct.description} </p>
+                        <button onclick="deleteProduct('${eachProduct._id}')">delete </button>
+                    </div>
+                    `
+            })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            document.querySelector("#result").innerHTML =
+                error.data.message
+        })
+}
+let deleteProduct = (id) => {
+
+    axios.delete(`${baseUrl}/product/${id}`)
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data);
+
+            document.querySelector("#result").innerHTML =
+                response.data.message
+
+            getAllProducts();
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            document.querySelector("#result").innerHTML =
+                error.data.message
+        })
+
+
+}
+
+
+
