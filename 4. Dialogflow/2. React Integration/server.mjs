ec6bc30cbdb14a1606907e690421b503219c8843
M Inzamam Malik <malikasinger1@gmail.com>
2023-03-19 22:30:23 +0500

    class code

--- 4. Dialogflow/2. React Integration/server.mjs
+++ 4. Dialogflow/2. React Integration/server.mjs
@@ -2,6 +2,16 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors'
 import mongoose from 'mongoose';
+import axios from 'axios'
+import dialogflow from 'dialogflow';
+import * as dotenv from 'dotenv'
+import momentTZ from 'moment-timezone';
+import morgan from "morgan"
+
+
+
+
+dotenv.config()
 
 let productSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -25,6 +35,8 @@ const messageModel = mongoose.model('messages', messageSchema);
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
+
 
 const port = process.env.PORT || 5001;
 
@@ -46,13 +58,48 @@ app.post("/message", async (req, res) => {
                 query: body.query,
                 from: "user"
             })
-            
+
 
         // TODO: send query to dialogflow and get chatbot response
 
+        // let response = await axios.post(
+        //     "https://dialogflow.googleapis.com/v2/projects/batch2helloworld-ogfk/agent/sessions/user123:detectIntent",
+        //     {
+        //         "queryInput": {
+        //             "text": {
+        //                 "text": body.query,
+        //                 "languageCode": 'en-us'
+        //             },
+        //         }
+        //     }
+        // )
+
+        const sessionClient = new dialogflow.SessionsClient();
+        const sessionPath = sessionClient.sessionPath("batch2helloworld-ogfk", "user123");
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: body.query,
+                    // The language used by the client (en-US)
+                    languageCode: 'en-US',
+                },
+            },
+            
+        };
+
+        // Send request and log result
+        const responses = await sessionClient.detectIntent(request);
+        console.log('Detected intent');
+        const result = responses[0].queryResult;
+
+        console.log("responses: ", result.fulfillmentText);
+
         res.send({
             message: {
-                text: "this is Hello from Chatbot"
+                text: result.fulfillmentText
             }
         })
 
@@ -63,6 +110,198 @@ app.post("/message", async (req, res) => {
         })
     }
 })
+
+
+app.post("/webhook", async (req, res) => {
+    try {
+        const body = req.body;
+
+        const intentName = body.queryResult.intent.displayName
+        const params = body.queryResult.parameters
+
+
+        switch (intentName) {
+            case 'Default Welcome Intent': {
+
+                const currentTime = momentTZ.tz(moment(), "Asia/Karachi");
+                const currentHour = +moment(currentTime).format('HH');
+                console.log("currentHour: ", currentHour);
+
+                let greeting = '';
+
+                if (currentHour < 6) {
+                    greeting = "good night"
+                } else if (currentHour < 12) {
+                    greeting = "good morning"
+                } else if (currentHour < 15) {
+                    greeting = "good afternoon"
+                } else if (currentHour < 17) {
+                    greeting = "good evening"
+                } else {
+                    greeting = "good night"
+                }
+
+                let responseText = pluck([
+                    `${greeting}! welcome to Abc Pizza store, how can I help you?`,
+                    "this is alternate response from webhook server",
+                    "this is third response"
+                ])
+
+                console.log(responseText);
+
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    responseText
+                                ]
+                            }
+                        }
+                    ]
+                })
+
+
+                break;
+            }
+            case 'newOrder': {
+
+                console.log("collected params: ", params);
+                console.log("collected params: ", params.person);
+
+                const newOrder = new orderModel({
+                    orderName: params.person.name,
+                    pizzaSize: params.pizzaSize,
+                    pizzaFlavour: params.pizzaFlavour,
+                    qty: params.qty
+                });
+                const savedOrder = await newOrder.save();
+                console.log(`New order added:`, savedOrder);
+
+
+                let responseText = `you said ${params.qty} ${params.pizzaSize} ${params.pizzaFlavour} pizza, your pizza is on the way. would you like to have cold drink?`
+
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    responseText
+                                ]
+                            }
+                        }
+                    ],
+                    "outputContexts": [
+                        {
+                            "name": `${req.body.session}/contexts/coolDrinkAsk`,
+                            "lifespanCount": 2,
+                            "parameters": {
+                                "param-name": "param-value"
+                            }
+                        }
+                    ]
+                })
+                console.log("session: ", `${req.body.session}/contexts/coolDrinkAsk`);
+
+                break;
+            }
+            case 'checkOrderStatus': {
+
+                let responseText = '';
+
+                const recentOrders = await orderModel.find({})
+                    .sort({ createdOn: -1 })
+                    .limit(15);
+
+                let latestPendingOrders = []
+                for (let i = 0; i < recentOrders.length; i++) {
+                    if (recentOrders[i].status === 'pending') {
+                        latestPendingOrders.push(recentOrders[i])
+                    } else {
+                        break
+                    }
+                }
+
+                if (latestPendingOrders.length === 0) {
+
+                    responseText =
+                        `${recentOrders[0].orderName}, your order for ${recentOrders[0].qty} ${recentOrders[0].pizzaSize} ${recentOrders[0].pizzaFlavour} pizza is ${recentOrders[0].status} ${moment(recentOrders[0].createdOn).fromNow()}`
+
+                } else {
+
+                    responseText += `${latestPendingOrders[0].orderName}, you have ${latestPendingOrders.length} pending ${latestPendingOrders.length > 1 ? "orders." : "order"}`
+
+                    latestPendingOrders.map((eachOrder, i) => {
+                        if (latestPendingOrders.length > 1) {
+                            responseText += ` order number ${i + 1},`
+                        } else {
+                            responseText += ` for`
+                        }
+
+                        responseText += ` ${eachOrder.qty} ${eachOrder.pizzaSize} ${eachOrder.pizzaFlavour} pizza,`
+                    })
+
+                    responseText += ` please be patient your order will be delivered soon.`
+                }
+
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    responseText
+                                ]
+                            }
+                        }
+                    ]
+                })
+
+                break;
+            }
+            default:
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    "sorry webhook dont know answer for this intent"
+                                ]
+                            }
+                        }
+                    ]
+                })
+                break;
+        }
+
+
+    } catch (e) {
+        console.error("Error adding order:", e);
+
+        res.send({
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "something is wrong in server, please try again"
+                        ]
+                    }
+                }
+            ]
+        })
+    }
+})
+
+let orderSchema = new mongoose.Schema({
+    orderName: { type: String, required: true },
+    pizzaSize: { type: String, required: true },
+    pizzaFlavour: { type: String, required: true },
+    qty: { type: Number, required: true },
+    status: { type: String, default: "pending" }, // canceled, inProgress delivered
+    createdOn: { type: Date, default: Date.now }
+});
+const orderModel = mongoose.model('orders', orderSchema);
+
+
 
 
 app.post("/product", (req, res) => {
